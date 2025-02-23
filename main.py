eff8c45c0730bd72de490045cb198a399832283e
sahasra560 <lpsahasra@gmail.com>
2025-02-23 16:48:10 +0530

    transformation completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception1 import NetworkSecurityException
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 
 if __name__=="__main__":
     try:
@@ -20,7 +21,13 @@ if __name__=="__main__":
         data_validation_artifact=data_validation.initiate_data_validation ( )
         logging.info ("data validation completed" )
         print(data_validation_artifact )
-
+        data_transformation_config=DataTransformationConfig (trainingpipelineconfig)
+        logging.info ( "data transformation started")
+        data_transformation=DataTransformation (data_validation_artifact,data_transformation_config )
+        data_transformation_artifact=data_transformation.initiate_data_transformation ( )
+        print (data_transformation_artifact )
+        logging.info("data Transformation completed")
+        
     
     except Exception as e:
         raise NetworkSecurityException(e,sys )
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -58,8 +58,7 @@ class DataTransformationConfig:
             training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"),)
         self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir,  training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
             training_pipeline.TEST_FILE_NAME.replace("csv", "npy"), )
-        self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
-            training_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
+        self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR)
         
 class ModelTrainerConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
