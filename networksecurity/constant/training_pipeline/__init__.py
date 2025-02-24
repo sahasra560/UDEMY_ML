04553bbe748ef35d11396de7eda6e227b7996f08
sahasra560 <lpsahasra@gmail.com>
2025-02-24 13:15:16 +0530

    model evaluation completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -11,7 +11,8 @@ TRAIN_FILE_NAME:str='train.csv'
 TEST_FILE_NAME:str='test.csv'
 
 SCHEMA_FILE_PATH=os.path.join("data_schema" ,"schema.yaml")
-
+SAVED_MODEL_DIR=os.path.join("saved_model" )
+MODEL_FILE_NAME='model.pkl'
 ''' data ingestion releated constant start with DATA_INGESTION varable name'''
 
 DATA_INGESTION_COLLECTION_NAME:str='NetworkData'
@@ -44,4 +45,11 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
 DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
 
 DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
+
+"""model trainer"""
+MODEL_TRAINER_DIR_NAME:str='model_trainer'
+MODEL_TRAINER_TRAINED_MODEL_DIR:str='trained_model'
+MODEL_TRAINER_TRAINED_MODEL_NAME:str='model.pkl'
+MODEL_TRAINER_EXPECTED_SCORE:float=0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD:float=0.05
 
--- networksecurity/utils/main_utlis/utils.py
+++ networksecurity/utils/main_utlis/utils.py
@@ -4,6 +4,8 @@ from networksecurity.logging.logger import logging
 import os,sys
 import numpy as np
 import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 def read_yaml_file(file_path:str )-> dict:
     try:
@@ -42,3 +44,52 @@ def save_object (file_path:str,obj:object )->None:
         logging.info( "existed the save object method")
     except Exception as e:
         raise NetworkSecurityException(e,sys )from e
+
+
+def load_object(file_path:str, )->object:
+    try:
+        if not os.path.exists(file_path ):
+            raise Exception(f"the file:{file_path} is not exists")
+        with open(file_path,"rb" )as file_obj:
+            print(file_path )
+            return pickle.load(file_obj )
+    except Exception as e:
+        raise NetworkSecurityException(e,sys ) from e
+
+def load_numpy_array_data(file_path:str )->np.array:
+    try:
+        with open(file_path,'rb' )as file_obj:
+            return np.load(file_obj )
+    except Exception as e:
+        raise NetworkSecurityException(e,sys )from e
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
